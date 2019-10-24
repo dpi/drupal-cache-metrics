f03fca3290ce1f84ef19366defc812023d6dc14c
Moshe Weitzman <weitzman@tejasa.com>
2019-10-24 12:23:51 -0400

    Ability to disable the NR piece.

--- src/CacheMetricsCacheTagsInvalidator.php
+++ src/CacheMetricsCacheTagsInvalidator.php
@@ -14,13 +14,20 @@ class CacheMetricsCacheTagsInvalidator implements CacheTagsInvalidatorInterface 
   protected $logger;
 
   /**
+   * @var bool
+   */
+  private $newRelicEnabled;
+
+  /**
    * CacheMetricsCacheTagsInvalidator constructor.
    *
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerChannelFactory
    *   A logger factory.
+   * @param bool $new_relic_enabled
    */
-  public function __construct(LoggerChannelFactoryInterface $loggerChannelFactory) {
+  public function __construct(LoggerChannelFactoryInterface $loggerChannelFactory, bool $new_relic_enabled) {
     $this->logger = $loggerChannelFactory->get('cache_metrics');
+    $this->newRelicEnabled = $new_relic_enabled;
   }
 
   /**
@@ -31,7 +38,7 @@ class CacheMetricsCacheTagsInvalidator implements CacheTagsInvalidatorInterface 
    */
   public function invalidateTags(array $tags) {
     $this->logger->debug(t('Invalidating the following tags: @tags', ['@tags' => implode(' ', $tags)]));
-    if (function_exists('newrelic_record_custom_event')) {
+    if ($this->isNewRelicEnabled()) {
       $this->logger->warning('NR *is* installed');
       // We don't use Monolog's NR handler because it just sets attributes on an existing event. See \Monolog\Handler\NewRelicHandler.
       // We can't record just one event because https://discuss.newrelic.com/t/how-to-send-multiple-items-in-a-custom-attribute/9280/5.
@@ -43,6 +50,15 @@ class CacheMetricsCacheTagsInvalidator implements CacheTagsInvalidatorInterface 
       // @todo remove
       $this->logger->warning('NR not installed');
     }
+  }
+
+  /**
+   * Use services.yml parameter to disable NR if you have the NR extension but dont want this logging.
+   *
+   * @return bool
+   */
+  public function isNewRelicEnabled() {
+    return $this->newRelicEnabled && function_exists('newrelic_record_custom_event');
   }
 
 }
